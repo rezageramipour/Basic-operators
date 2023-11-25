1e5f6e3bf7b5c602e705274aec97aa57d469004b
rezageramipour <rezageramipour7@gmail.com>
2023-11-25 09:12:58 -0800

    tamrin1

---
+++ static/assets/js/script.js
@@ -0,0 +1,12 @@
+var number1 = prompt ("عدد اول", "1");
+var number2 = prompt ("عدد دوم", "3");
+var number1TypeNumber = +(number1);
+var number2TypeNumber = +(number2);
+var zarb = number1TypeNumber * number2TypeNumber;
+var tagsim = number1TypeNumber / number2TypeNumber;
+var Plus = number1TypeNumber + number2TypeNumber;
+var minus = number1TypeNumber - number2TypeNumber;
+alert (zarb);
+alert (tagsim);
+alert (Plus);
+alert (minus);
